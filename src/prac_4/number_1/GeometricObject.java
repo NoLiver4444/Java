3571c7022c5b1520844376efa2b2119ab2b0155a
Пользователь <dprslt@inbox.ru>
2025-10-25 07:36:12 +0300

    qwer

---
+++ src/prac_4/number_1/GeometricObject.java
@@ -0,0 +1,34 @@
+package prac_4.number_1;
+
+public class GeometricObject {
+    private String color = "white";
+    private boolean filled = false;
+
+    public GeometricObject() {}
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Color: " + color + "\nIs filled: " + filled;
+    }
+}
